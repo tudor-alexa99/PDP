a7a2337747caf0f40906806bffe7d4e6710d958e
Tudor Alexa <tudor.alexa99@gmail.com>
2020-11-04 00:24:40 +0200

    Add Thread pool mechanism

--- Lab3/src/Main.java
+++ Lab3/src/Main.java
@@ -1,5 +1,34 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+//        Initialise the matrix
+        int totalThreads = 16;
+
+        int matrix_size = 1000;
+        int[][] A = new int[matrix_size][matrix_size];
+        int[][] B = new int[matrix_size][matrix_size];
+
+        for (int i = 0; i < matrix_size; i++) {
+            for (int j = 0; j < matrix_size; j++) {
+                // generate random values between 1 and 10 for the matrix
+                A[i][j] = (int) (1 + Math.random() * 10);
+                B[i][j] = (int) (1 + Math.random() * 10);
+            }
+        }
+
+        Program program = new Program(matrix_size, totalThreads, A, B);
+
+        program.simpleThreadsTask1();
+        program.simpleThreadsTask2();
+        program.simpleThreadsTask3();
+
+        program.printResult("\n\n");
+
+        program.threadPoolTask1();
+        program.threadPoolTask2();
+        program.threadPoolTask3();
+
+        program.printResult("\n\n");
     }
 }
--- Lab3/src/Program.java
+++ Lab3/src/Program.java
@@ -1,2 +1,164 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Program {
+    private int size;
+    private int totalThreads;
+    private int[][] A;
+    private int[][] B;
+
+    public Program(int size, int totalThreads, int[][] a, int[][] b) {
+        this.size = size;
+        this.totalThreads = totalThreads;
+        A = a;
+        B = b;
+    }
+
+    public void simpleThreadsTask1() throws InterruptedException, IOException {
+        MultiplyMatrix multiplyMatrix = new MultiplyMatrix(size, totalThreads, A, B);
+        long startingTime = System.currentTimeMillis();
+
+        List<Thread> threadList = new ArrayList<>();
+
+//        Create the threads
+        for (int i = 0; i < totalThreads; i++) {
+            threadList.add(new Thread(multiplyMatrix.task1));
+        }
+
+//        Start the threads
+        for (Thread thread : threadList)
+            thread.start();
+
+//        Join the threads
+        for (Thread thread : threadList)
+            thread.join();
+
+        long endingTime = System.currentTimeMillis();
+
+        printResult("Task 1 simple threads execution time: " + (endingTime - startingTime) +
+                " Matrix size: " + size +
+                " Number of threads: " + totalThreads +
+                "\n");
+
+    }
+
+    public void simpleThreadsTask2() throws InterruptedException, IOException {
+        MultiplyMatrix multiplyMatrix = new MultiplyMatrix(size, totalThreads, A, B);
+        long startingTime = System.currentTimeMillis();
+
+        List<Thread> threadList = new ArrayList<>();
+
+//        Create the threads
+        for (int i = 0; i < totalThreads; i++) {
+            threadList.add(new Thread(multiplyMatrix.task2));
+        }
+
+//        Start the threads
+        for (Thread thread : threadList)
+            thread.start();
+
+//        Join the threads
+        for (Thread thread : threadList)
+            thread.join();
+
+        long endingTime = System.currentTimeMillis();
+
+        printResult("Task 2 simple threads execution time: " + (endingTime - startingTime) +
+                " Matrix size: " + size +
+                " Number of threads: " + totalThreads +
+                "\n");
+
+    }
+
+    public void simpleThreadsTask3() throws InterruptedException, IOException {
+        MultiplyMatrix multiplyMatrix = new MultiplyMatrix(size, totalThreads, A, B);
+        long startingTime = System.currentTimeMillis();
+
+        List<Thread> threadList = new ArrayList<>();
+
+//        Create the threads
+        for (int i = 0; i < totalThreads; i++) {
+            threadList.add(new Thread(multiplyMatrix.task3));
+        }
+
+//        Start the threads
+        for (Thread thread : threadList)
+            thread.start();
+
+//        Join the threads
+        for (Thread thread : threadList)
+            thread.join();
+
+        long endingTime = System.currentTimeMillis();
+
+        printResult("Task 3 simple threads execution time: " + (endingTime - startingTime) +
+                " Matrix size: " + size +
+                " Number of threads: " + totalThreads +
+                "\n");
+
+    }
+
+
+    public void threadPoolTask1() throws IOException, InterruptedException {
+        MultiplyMatrix multiplyMatrix = new MultiplyMatrix(size, totalThreads, A, B);
+        ExecutorService executorService = Executors.newFixedThreadPool(totalThreads);
+
+        long startingTime = System.currentTimeMillis();
+        executorService.execute(multiplyMatrix.task1);
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        long endingTime = System.currentTimeMillis();
+
+        printResult("Task 1 Thread Pool execution time: " + (endingTime - startingTime) +
+                " Matrix size: " + size +
+                " Number of threads: " + totalThreads +
+                "\n");
+    }
+
+    public void threadPoolTask2() throws IOException, InterruptedException {
+        MultiplyMatrix multiplyMatrix = new MultiplyMatrix(size, totalThreads, A, B);
+        ExecutorService executorService = Executors.newFixedThreadPool(totalThreads);
+
+        long startingTime = System.currentTimeMillis();
+        executorService.execute(multiplyMatrix.task2);
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        long endingTime = System.currentTimeMillis();
+
+        printResult("Task 2 Thread Pool execution time: " + (endingTime - startingTime) +
+                " Matrix size: " + size +
+                " Number of threads: " + totalThreads +
+                "\n");
+    }
+
+    public void threadPoolTask3() throws IOException, InterruptedException {
+        MultiplyMatrix multiplyMatrix = new MultiplyMatrix(size, totalThreads, A, B);
+        ExecutorService executorService = Executors.newFixedThreadPool(totalThreads);
+
+        long startingTime = System.currentTimeMillis();
+        executorService.execute(multiplyMatrix.task3);
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        long endingTime = System.currentTimeMillis();
+
+        printResult("Task 3 Thread Pool execution time: " + (endingTime - startingTime) +
+                " Matrix size: " + size +
+                " Number of threads: " + totalThreads +
+                "\n");
+    }
+
+
+    public static void printResult(String output) throws IOException {
+        BufferedWriter writer = new BufferedWriter(
+                new FileWriter("output.txt", true));
+
+        writer.write(output);
+        writer.close();
+    }
 }
